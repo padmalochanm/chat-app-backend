15a72c8b44af72251ad8b055bb1117876b4630a6
Padmalochan <padmalochanmaiti33@gmail.com>
2024-07-07 17:30:31 +0530

    updated url

--- socket/socket.js
+++ socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Update to your frontend URL
+    origin: "https://chat-app-frontend-ashen-iota.vercel.app", // Update to your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
